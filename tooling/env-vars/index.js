4973f5ddccb3b9bc2a9335db8061b44a4870cb42
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-02-23 20:14:48 -0800

    remove external links

--- tooling/env-vars/index.js
+++ tooling/env-vars/index.js
@@ -122,26 +122,12 @@ module.exports = ({
     hot,
     htmlInjections: {
       favicon: getFavicon(appEnv),
-      sentry: isProduction
-        ? `<script
-      src="https://js.sentry-cdn.com/f1a3d53e530e465e8f74f847370b594b.min.js"
-      crossorigin="anonymous"
-      data-lazy="no"
-    ></script>`
-        : '',
-      bangleHelpPreload: `<link
-        rel="preload"
-        href="https://unpkg.com/bangle-io-help@${helpDocsVersion}/docs/getting%20started.md"
-        as="fetch"
-        crossorigin
-      />`,
+      sentry: '',
+      bangleHelpPreload: '',
       viteJsEntry: isVite
         ? '<script type="module" src="/app/app-entry/index.ts"></script>'
         : '',
-      fathom:
-        appEnv === 'production'
-          ? `<script defer src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="AOGIPUKY"></script>`
-          : '',
+      fathom: ''
     },
     appEnvs: {
       'process.env.__BANGLE_BUILD_TIME_CONFIG__': JSON.stringify(
